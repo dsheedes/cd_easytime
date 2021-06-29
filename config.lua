d4195e6313ed16d0b3b2db87a5d1c55b8acd8d2b
Rampodious the 2nd <ramp.clash@gmail.com>
2021-06-29 03:44:36 +0100

    easytime - small config change

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ Config.Framework_perms = { --You can choose multiple permission group to have ac
 Config.Command = 'easytime' --Customise the command name to open the UI.
 Config.Notification_Type = 'chat' --[ 'chat' / 'mythic_old' / 'mythic_new' / 'esx' / 'custom' ] Choose your notification type.
 Config.Language = 'EN' --[ 'EN' / 'FR' / 'ES' ] Choose your preferred language.
-Config.NUI_keepinput = false --Do you want to be able to walk around when the UI is open?
+Config.NUI_keepinput = true --Do you want to be able to walk around when the UI is open?
 
 Config.TimeCycleSpeed = 2 --(in seconds) Changing this value will effects the day/night time cycle, decreasing slows it down, incresing speeds it up. Right now its similar to the default gta5 time cycle.
 Config.DynamicWeather = true --Do you want to allow dynamic weather?
